50e3b5a9e447d2ceb7db711305352414b2f0b581
armadhilo <dunsforce@gmail.com>
2020-12-24 17:38:04 +0700

    add env

---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'ebroker-map',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  },
+
+  env: {
+    GRAPH_TOKEN: process.env.GRAPH_TOKEN,
+    MAP_BOX_TOKEN: process.env.MAP_BOX_TOKEN,
+    API_URL: process.env.API_URL
+  }
+}
